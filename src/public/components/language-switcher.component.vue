0356f1bd598fdaae2feec45926a39d3e421c5d0a
Ro <u202213765@upc.edu.pe>
2025-05-05 17:23:12 -0500

    feat: add pages.

---
+++ src/public/components/language-switcher.component.vue
@@ -0,0 +1,29 @@
+<script>
+import {SelectButton as PvSelectButton} from "primevue";
+import {defaultOptions as $i18n} from "@primevue/core/config";
+
+export default {
+  name: "language-switcher",
+  components: {PvSelectButton},
+  data(){
+    return {
+      languages:[]
+    }
+  },
+  created(){
+    this.languages = this.$i18n.availableLocales;
+  }
+}
+</script>
+
+<template>
+  <pv-select-button v-model="$i18n().locale" :options="languages">
+    <template #option="slotProps">
+      {{slotProps.option.toUpperCase()}}
+    </template>
+  </pv-select-button>
+</template>
+
+<style scoped>
+
+</style>
